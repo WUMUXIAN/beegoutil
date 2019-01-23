500caf21c1901b3e2f0bac1ff42c43efc3727a9c
Wu Muxian <wumuxian@sensetime.com>
2019-01-23 14:18:33 +0800

    removed the concrete type, only leave the interface

--- util.go
+++ util.go
@@ -4,26 +4,10 @@ import (
 	"github.com/astaxie/beego"
 )
 
-// StatusMessageModel defines the StatusMessageModel body
-type StatusMessageModel struct {
-	Status  int    `json:"status"`
-	Message string `json:"message,omitempty"`
-}
-
 // StatusMessageModelInterface defines the StatusMessageModeInterface
 type StatusMessageModelInterface interface {
 	SetStatus(status int)
 	SetMessage(message string)
-}
-
-// SetStatus sets the response status
-func (r *StatusMessageModel) SetStatus(status int) {
-	r.Status = status
-}
-
-// SetMessage sets the response error message
-func (r *StatusMessageModel) SetMessage(message string) {
-	r.Message = message
 }
 
 // ResponseIfError responds the client with given status and message if error is encountered
